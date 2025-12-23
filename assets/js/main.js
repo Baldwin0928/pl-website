86ca973e087e3677314a4c5a95a18b024092c8dd
Chen Szu Heng <108592645+Baldwin0928@users.noreply.github.com>
2025-12-23 14:35:49 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,7 +169,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // TUNING
   const START_BEFORE_SECTION_VH = 0.70; // start when you're a bit before the quote section
-  const END_BEFORE_MISSION_VH = 0.20;   // finish right before Our Mission shows (smaller => later)
+  const END_BEFORE_MISSION_VH = 0.35;   // finish right before Our Mission shows (smaller => later)
   const BUMP_PX = 12;
   const BASE_OPACITY = 0.14;
 
@@ -237,4 +237,5 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("resize", onScroll);
   onScroll();
 })();
+
 
